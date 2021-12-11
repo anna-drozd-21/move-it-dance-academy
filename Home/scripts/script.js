1c28b155af7ee8e044909012807eac9a17ba5a52
Anna Drozd <anndrozd1010@gmail.com>
2021-12-11 16:33:38 +0000

    combined CSS - 1st attempt

--- Home/scripts/script.js
+++ Home/scripts/script.js
@@ -1,4 +1,4 @@
-//sale button functions
+//sale button behaviour
 function saleButtonSmall(b) {
   b.style.height = "86px";
   b.style.width = "166px";
@@ -7,29 +7,12 @@ function saleButtonBig(b) {
   b.style.height = "106px";
   b.style.width = "186px";
 }
-
-
-/*
-//slideshow - Array
-var i=0;
-var arrayOfImages=[];
-var change=4000;
-
-//populate the array of images
-arrayOfImages[0]="review1.png";
-arrayOfImages[1]="review2.png";
-
-//function that will change the images
-function movingSlides(){
-document.slideshow.src=arrayOfImages[i];
-	if (i<arrayOfImages.length-1){
-		i++;
-	}
-	else{
-		i=0;	
-	}
+//sale button link to Booking page
+function linkToBooking(){
+	window.open("Book a Class/index.html", '_blank');
 	
-	setTimeout("movingSlides()",change);
-}
-window.onload=movingSlides;
-*/
+} 
+
+  
+
+
